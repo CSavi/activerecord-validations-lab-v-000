43ed2afd5bb6d90d69568dce9bff4d45a38f07fe
Carrie Savinell <carrielane89@gmail.com>
2018-08-01 14:54:53 +0000

    automatically backed up by learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,6 @@ class Post < ActiveRecord::Base
   validates :title, presence: true
   validates :content, length: { minimum: 250 }
   validates :summary, length: { maximum: 250 }
-  validates :category, inclusion: { in %w( fiction non-fiction),
+  validates :category, inclusion: { in ( fiction non-fiction),
     message: "%{value} is not a valid category"}
 end
